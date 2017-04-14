5cb8c73bf46969963eaa01199439ad1ba0a98087
bravo <nishantbansal2509@gmail.com>
2017-04-14 06:47:02 +0530

    game graphics incomplete

--- src/client/index.c
+++ src/client/index.c
@@ -5,7 +5,69 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32;
+const int SPRITE_SIZE = 32;
 
+
+/* source and destination rectangles */
+SDL_Rect rcSrc, rcSprite;
+int xGlobal, yGlobal;
+int gameover = 0;
+
+void HandleEvent(SDL_Event event)
+{
+    switch (event.type) {
+        /* close button clicked */
+        case SDL_QUIT:
+            gameover = 1;
+            break;
+            
+        /* handle the keyboard */
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                case SDLK_q:
+                    gameover = 1;
+                    break;
+                case SDLK_LEFT:
+                    if ( rcSrc.x == 192 )
+                        rcSrc.x = 224;
+                    else
+                        rcSrc.x = 192;
+                    rcSprite.x -= 5;
+                    if (xGlobal - rcSprite.x >= 160)
+                    {
+                        xGlobal = xGlobal - 160 + rcSprite.x;
+                    }
+                    break;
+                case SDLK_RIGHT:
+                    if ( rcSrc.x == 64 )
+                        rcSrc.x = 96;
+                    else
+                        rcSrc.x = 64;
+                    rcSprite.x += 5;
+                    if (rcSprite.x - xGlobal >= 160)
+                    {
+                        xGlobal = xGlobal - 480 + rcSprite.x;
+                    }
+                    break;
+                case SDLK_UP:
+                    if ( rcSrc.x == 0 )
+                        rcSrc.x = 32;
+                    else
+                        rcSrc.x = 0;
+                    rcSprite.y -= 5;
+                    break;
+                case SDLK_DOWN:
+                    if ( rcSrc.x == 128 )
+                        rcSrc.x = 160;
+                    else
+                        rcSrc.x = 128;
+                    rcSprite.y += 5;
+                    break;
+            }
+            break;
+    }
+}
 // SDL_Event event;
 // TTF_Font * font = NULL;
 // SDL_Color textColor = { 0xFF, 0xFF, 0xFF };
@@ -20,8 +82,8 @@ struct clientInfo{
 // };
 
 struct stringInput{
-	char str[20];
-	SDL_Surface * text;
+    char str[20];
+    SDL_Surface * text;
 };
 
 SDL_Surface * load_image( char * filename )
@@ -138,27 +200,27 @@ int main(int argc, char const *argv[])
     char result[100]= {'\0'};
     int loginReturn;
 
-	int quit = 0;
+    int quit = 0;
 
-	SDL_Surface * welcome = NULL;
-	SDL_Surface * screen = NULL;
-	SDL_Surface *message1 = NULL;
-	SDL_Surface *message2 = NULL;
-	SDL_Surface *message3 = NULL;
+    SDL_Surface * welcome = NULL;
+    SDL_Surface * screen = NULL;
+    SDL_Surface *message1 = NULL;
+    SDL_Surface *message2 = NULL;
+    SDL_Surface *message3 = NULL;
 
-	struct stringInput * name = (struct stringInput *) malloc (sizeof(struct stringInput));
-	name->text == NULL;
-	memset(name->str,'\0',20);
-	int nameEntered = 0;
+    struct stringInput * name = (struct stringInput *) malloc (sizeof(struct stringInput));
+    name->text == NULL;
+    memset(name->str,'\0',20);
+    int nameEntered = 0;
 
-	struct stringInput * pwd = (struct stringInput *) malloc (sizeof(struct stringInput));
-	pwd->text == NULL;
-	memset(pwd->str,'\0',20);
-	int pwdEntered = 1;
+    struct stringInput * pwd = (struct stringInput *) malloc (sizeof(struct stringInput));
+    pwd->text == NULL;
+    memset(pwd->str,'\0',20);
+    int pwdEntered = 1;
 
-	//start SDL
-	SDL_Init (SDL_INIT_EVERYTHING);
-	TTF_Init();
+    //start SDL
+    SDL_Init (SDL_INIT_EVERYTHING);
+    TTF_Init();
     if( Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 ) == -1 )
     {
         printf("Error initializing music stream...\n");
@@ -173,13 +235,13 @@ int main(int argc, char const *argv[])
         printf("Error loading music...\n");
         return 0;
     }
-	//set up screen
-	screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
-	//Set the window caption
+    //set up screen
+    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
+    //Set the window caption
     SDL_WM_SetCaption( "Welcome to Pokemon MMORPG", NULL );
 
     font = TTF_OpenFont( "./../src/client/GUIGraphics/fontb.ttf", 40 );
-	welcome = load_image( "./../src/client/GUIGraphics/pokelogo.jpg" );
+    welcome = load_image( "./../src/client/GUIGraphics/pokelogo.bmp" );
 
     if (font == NULL || welcome == NULL)
     {
@@ -187,11 +249,11 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
-	message1 = TTF_RenderText_Solid( font, "Welcome to Pokemon MMORPG !!!", textColor );
-	message2 = TTF_RenderText_Solid( font, "Enter your username:", textColor );
+    message1 = TTF_RenderText_Solid( font, "Welcome to Pokemon MMORPG !!!", textColor );
+    message2 = TTF_RenderText_Solid( font, "Enter your username:", textColor );
 
-	//update Screen
-	SDL_Flip(screen);
+    //update Screen
+    SDL_Flip(screen);
 
     if( Mix_PlayMusic( music, -1 ) == -1 )
     {
@@ -199,16 +261,16 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-	//While the user hasn't quit
+    //While the user hasn't quit
     while( quit == 0 )
     {
         //While there's an event to handle
         while( SDL_PollEvent( &event ) )
         {
-        	SDL_EnableUNICODE(1);
+            SDL_EnableUNICODE(1);
 
-        	// if( 1 != 1)
-        	if( pwdEntered == 0 && nameEntered == 1)
+            // if( 1 != 1)
+            if( pwdEntered == 0 && nameEntered == 1)
             {
                 //Get user input
                 handle_input(pwd, event, font, textColor);
@@ -223,20 +285,20 @@ int main(int argc, char const *argv[])
                     message2 = TTF_RenderText_Solid( font, "Connecting to the serever...", textColor );
                     loginReturn = login("login", "name", name->str, pwd->str);
                     if ( loginReturn == 1){
-                    	strcpy(result, "Logged in succesfully!");
-                    	// printf("%s\n", result);
+                        strcpy(result, "Logged in succesfully!");
+                        // printf("%s\n", result);
                     }
                     else if (loginReturn == 0)
                     {
-                    	strcpy(result, "Username and/or password Incorrect. Please try again!");
-                    	// printf("%s\n", result);
+                        strcpy(result, "Username and/or password Incorrect. Please try again!");
+                        // printf("%s\n", result);
                     }
-                    font = TTF_OpenFont( "./../data/GUI/fontb.ttf", 20 );
+                    font = TTF_OpenFont( "./../src/client/GUIGraphics/fontb.ttf", 20 );
                     message3 = TTF_RenderText_Solid( font, result, textColor);
                     quit = 1;
                 }
             }
-        	if( nameEntered == 0 )
+            if( nameEntered == 0 )
             {
                 //Get user input
                 handle_input(name, event, font, textColor);
@@ -266,20 +328,20 @@ int main(int argc, char const *argv[])
         apply_surface( ( SCREEN_WIDTH - message1->w ) / 2, ( ( SCREEN_HEIGHT / 3 ) - message1->h ) / 2, message1, screen );
         apply_surface( ( SCREEN_WIDTH - message2->w ) / 2, ( ( SCREEN_HEIGHT / 2 ) - message2->h ) / 2, message2, screen );
         if( message3 != NULL )
-	    {
-	        //Show the name
-	        apply_surface( ( SCREEN_WIDTH - message3->w ) / 2, ( ( SCREEN_HEIGHT  ) - message3->h ) / 2, message3, screen );
-	    }
+        {
+            //Show the name
+            apply_surface( ( SCREEN_WIDTH - message3->w ) / 2, ( ( SCREEN_HEIGHT  ) - message3->h ) / 2, message3, screen );
+        }
         if( name->text != NULL )
-	    {
-	        //Show the name
-	        apply_surface( ( SCREEN_WIDTH - (name->text)->w ) / 2, ( SCREEN_HEIGHT - (name->text)->h ) / 2, name->text, screen );
-	    }
-	    if( pwd->text != NULL )
-	    {
-	        //Show the pwd
-	        apply_surface( ( SCREEN_WIDTH - (pwd->text)->w ) / 2, ( SCREEN_HEIGHT - (pwd->text)->h ) / 2, pwd->text, screen );
-	    }
+        {
+            //Show the name
+            apply_surface( ( SCREEN_WIDTH - (name->text)->w ) / 2, ( SCREEN_HEIGHT - (name->text)->h ) / 2, name->text, screen );
+        }
+        if( pwd->text != NULL )
+        {
+            //Show the pwd
+            apply_surface( ( SCREEN_WIDTH - (pwd->text)->w ) / 2, ( SCREEN_HEIGHT - (pwd->text)->h ) / 2, pwd->text, screen );
+        }
         if( SDL_Flip( screen ) == -1 )
         {
             return 1;
@@ -288,19 +350,19 @@ int main(int argc, char const *argv[])
     }
     if (loginReturn == 0)
     {
-    	sleep(4);
-    	SDL_FreeSurface (welcome);
-	    SDL_FreeSurface( message1 );
-	    SDL_FreeSurface( message2 );
-	    SDL_FreeSurface( message3 );
+        sleep(4);
+        SDL_FreeSurface (welcome);
+        SDL_FreeSurface( message1 );
+        SDL_FreeSurface( message2 );
+        SDL_FreeSurface( message3 );
         Mix_FreeMusic( music );
-	    //Close the font that was used
-	    TTF_CloseFont( font );
-	    TTF_Quit();
-		SDL_Quit();
-		return 0;
+        //Close the font that was used
+        TTF_CloseFont( font );
+        TTF_Quit();
+        SDL_Quit();
+        return 0;
     }
-	//free the loaded image
+    //free the loaded image
     SDL_FreeSurface (welcome);
     SDL_FreeSurface( message1 );
     SDL_FreeSurface( message2 );
@@ -308,24 +370,87 @@ int main(int argc, char const *argv[])
     //Close the font that was used
     TTF_CloseFont( font );
 
-	printf("now I have to implement game graphics.\n");
+    printf("now I have to implement game graphics.\n");
 
     if( SDL_Flip( screen ) == -1 )
     {
         return 1;
     }
 
-    // SDL_Surface * gameScreen = NULL;
-    screen = SDL_SetVideoMode( SCREEN_WIDTH + 160, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
+    SDL_Surface * game = load_image("./../src/client/GUIGraphics/game.jpg");
+    SDL_Surface * sprite = load_image("./../src/client/GUIGraphics/sprite.bmp");
+    int colorkey = SDL_MapRGB(screen->format, 255, 0, 255);
+    SDL_SetColorKey(sprite, SDL_SRCCOLORKEY | SDL_RLEACCEL, colorkey);
+
+
+
+    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
     SDL_WM_SetCaption( "Start your quest in Pokemon MMORPG", NULL );
-    sleep(2);
+    SDL_EnableKeyRepeat(70, 70);
 
+    int x = 320, y = 240;
 
+    //get x y
 
+    xGlobal = x;
+    yGlobal = y;
+    apply_surface(xGlobal-(SCREEN_WIDTH/2),yGlobal-(SCREEN_HEIGHT/2),game,screen);
+
+    /* set sprite position */
+    rcSprite.x = x;
+    rcSprite.y = y;
+
+    /* set animation frame */
+    rcSrc.x = 128;
+    rcSrc.y = 0;
+    rcSrc.w = SPRITE_SIZE;
+    rcSrc.h = SPRITE_SIZE;
+
+    gameover = 0;
+
+    /* message pump */
+    while (!gameover)
+    {
+        SDL_Event event;
+        
+        /* look for an event */
+        if (SDL_PollEvent(&event)) {
+            HandleEvent(event);
+        }
+
+        /* collide with edges of screen */
+        // if (rcSprite.x <= 0)
+        //     rcSprite.x = 0;
+        // if (rcSprite.x >= SCREEN_WIDTH - SPRITE_SIZE) 
+        //     rcSprite.x = SCREEN_WIDTH - SPRITE_SIZE;
+
+        // if (rcSprite.y <= 0)
+        //     rcSprite.y = 0;
+        // if (rcSprite.y >= SCREEN_HEIGHT - SPRITE_SIZE) 
+        //     rcSprite.y = SCREEN_HEIGHT - SPRITE_SIZE;
+
+        /* draw the grass */
+        // for (int x = 0; x < SCREEN_WIDTH / SPRITE_SIZE; x++) {
+        //     for (int y = 0; y < SCREEN_HEIGHT / SPRITE_SIZE; y++) {
+        //         rcGrass.x = x * SPRITE_SIZE;
+        //         rcGrass.y = y * SPRITE_SIZE;
+        //         SDL_BlitSurface(grass, NULL, screen, &rcGrass);
+        //     }
+        // }
+
+        apply_surface(xGlobal-(SCREEN_WIDTH/2),yGlobal-(SCREEN_HEIGHT/2),game,screen);
+        /* draw the sprite */
+        SDL_BlitSurface(sprite, &rcSrc, screen, &rcSprite);
+
+        /* update the screen */
+        SDL_UpdateRect(screen, 0, 0, 0, 0);
+    }
+
+    // pause();
     // SDL_FreeSurface(gameScreen);
     Mix_FreeMusic( music );
     TTF_Quit();
-	SDL_Quit();
+    SDL_Quit();
 
-	return 0;
+    return 0;
 }
