d0c44c0dc80324e0a5cbda9548368d98297041d4
bravo <nishantbansal2509@gmail.com>
2017-04-15 17:32:26 +0530

    player movement along with world movement added

--- src/utils/hash.c
+++ src/utils/hash.c
@@ -9,25 +9,23 @@ char * hash(char * password){
 
 	char * hashedPassword;
 	int i, passwordLen, sum = 0, A[8];
-
+	
 	hashedPassword = malloc(9 * sizeof(char));
 	passwordLen = strlen(password);
 	for(i=0;i<passwordLen;i++){
 		sum += password[i];
 	}
-	A[0] = rand() % 100;
-	A[1] = rand() % 100;
-	A[2] = rand() % 100;
-	A[3] = rand() % 100;
-	A[4] = rand() % 100;
-	A[5] = rand() % 100;
-	A[6] = rand() % 100;
-	A[7] = rand() % 100;
-
+	A[0] = 36;
+	A[1] = 28;
+	A[2] = 35;
+	A[3] = 49;
+	A[4] = 1;
+	A[5] = 22;
+	A[6] = 57;
+	A[7] = 24;
 	for(i=0;i<8;i++){
 		hashedPassword[i] = 65 + (((password[i]*A[i])%100)%26);
 	}
-	
 	hashedPassword[i] = '\0';
 	return hashedPassword;
 }
